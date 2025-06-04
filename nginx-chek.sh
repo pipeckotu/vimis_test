e9fa859e53d367a08dcd9b139191cd6c07a89a3a
pipeckotu <pipeckotu@gmail.com>
2025-06-04 11:05:13 +0700

    форматирование

--- nginx-chek.sh
+++ nginx-chek.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 for run in {1..13}; do
-UP=$(netstat -tulpn | grep :80 | grep dockerd | wc -l);
-if [ "$UP" -ne 1 ];
-then
-#        sudo service nginx start
-        echo "Webserver Is DOWN";
-else
-        echo "Webserver Is UP";
-fi
-sleep 5s
+  UP=$(netstat -tulpn | grep :80 | grep dockerd | wc -l);
+  if [ "$UP" -ne 1 ];
+    then
+      echo "Webserver Is DOWN";
+    else
+      echo "Webserver Is UP";
+    fi
+  sleep 5s
 done
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- d=$(dirname $0)
+d=$(dirname $0)
   docker stack deploy -c docker-test.yml test --detach=false
- sleep 5s
-  . ${d}/nginx-chek.sh
+sleep 5s
+. ${d}/nginx-chek.sh
