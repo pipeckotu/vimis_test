bb282913a6a7c8202603aedcc181322aaf3e3b94
Alex Ku <pipeckotu@gmail.com>
2025-06-04 14:48:08 +0700

    empty

--- nginx-chek-all.sh
+++ nginx-chek-all.sh
@@ -1,11 +1,1 @@
 #!/bin/bash
-for run in {1..5}; do
- UP=$(curl -I http://10.15.25.55 -s |grep HTTP |grep 200 |wc -l);
-  if [ "$UP" -ne 1 ];
-    then
-      echo "Webserver Is DOWN";
-    else
-      echo "Webserver Is UP";
-    fi
-  sleep 5s
-done
